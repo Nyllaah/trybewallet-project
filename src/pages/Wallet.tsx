9f7aba722dbf6898f535b25346edce3657da79fe
Nyllaah <nylla.g@hotmail.com>
2023-10-04 18:10:33 -0300

    feat: req 2

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  const { email } = useSelector((state: { user: { email: string } }) => state.user);
+  const [total, setTotal] = useState(0);
+
+  return (
+    <>
+      <header>
+        <span data-testid="email-field">{`Email: ${email}`}</span>
+        <span data-testid="total-field">{`Despesa total: ${total}`}</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    </>
+  );
 }
 
 export default Wallet;
